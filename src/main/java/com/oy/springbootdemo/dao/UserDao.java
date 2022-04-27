708a33b7ad5de34d89de0266f4a3b5d8972ea651
ouyangsicong <ouyangsicong@foxmail.com>
2022-04-27 22:33:00 +0800

    git2

--- src/main/java/com/oy/springbootdemo/dao/UserDao.java
+++ src/main/java/com/oy/springbootdemo/dao/UserDao.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.oy.springbootdemo.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
 
 @Mapper
 public interface UserDao extends BaseMapper<User> {
