74221c9c490b7eb11f4b9e103efa6c3ab4dec886
ouyangsicong <ouyangsicong@foxmail.com>
2022-07-18 21:08:47 +0800

    修改配置，使用单元测试

--- src/main/java/com/oy/springbootdemo/dao/UserDao.java
+++ src/main/java/com/oy/springbootdemo/dao/UserDao.java
@@ -3,10 +3,11 @@ package com.oy.springbootdemo.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.oy.springbootdemo.entity.User;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Mapper
 public interface UserDao extends BaseMapper<User>  {
-    void selectUser();
+    List<User> selectUser();
 }
